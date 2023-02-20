6a113c51cf04c7dfa395e9e3c5710773057218b8
sofievd <sofievandingenen@hotmail.com>
2023-02-20 14:53:55 +0100

    staring to solve the assignment.

--- Module5-Solution/js/script.js
+++ Module5-Solution/js/script.js
@@ -83,7 +83,9 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
         showLoading("#main-content");
         $ajaxUtils.sendGetRequest(
             allCategoriesUrl,
-            [function...], // ***** <---- TODO: STEP 1: Substitute [...] ******
+            [function (responseText) {
+                document.querySelector("#main-content").innerHTML = responseText
+            }], // ***** <---- TODO: STEP 1: Substitute [...] ******
             true); // Explicitly setting the flag to get JSON from server processed into an object literal
     });
 // *** finish **
@@ -96,12 +98,19 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
         // Load home snippet page
         $ajaxUtils.sendGetRequest(
             homeHtmlUrl,
-            function (homeHtml) {
+            function (homeHtmlUrl) {
+
+                var chosenCategoryShortName = chooseRandomCategory(categories);
+                chosenCategoryShortName += chosenCategoryShortName.short_name;
+
 
                 // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
                 // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
                 // variable's name implies it expects.
                 // var chosenCategoryShortName = ....
+
+                 var  homeHtmlToInsertIntoMainPage = homeHtmlUrl;
+                homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlToInsertIntoMainPage, "randomCategoryShortName", chosenCategoryShortName);
 
 
                 // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -117,6 +126,7 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
                 //
                 // var homeHtmlToInsertIntoMainPage = ....
 
+                return  homeHtmlToInsertIntoMainPage
 
                 // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
                 // Use the existing insertHtml function for that purpose. Look through this code for an example
